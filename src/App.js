bc4d6241d4fd87f202befd8453022a96fd7e9ce8
JT <jathompson@mdsol.com>
2021-03-30 15:25:49 -0700

    Changes to copy

--- src/App.js
+++ src/App.js
@@ -46,6 +46,9 @@ if (parseQS['CS-AUTO-ANSWERS']) {
   csAutoAnswer = true;
 }
 
+const csAgreed =
+  typeof window !== 'undefined' && window.CS_AGREED ? window.CS_AGREED : false;
+
 const validateEmail = (email) => {
   // eslint-disable-next-line no-control-regex
   const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
@@ -330,7 +333,7 @@ function App() {
   const Recommendations =
     formType === 'spb' ? spbRecommendations : mwmRecommendations;
 
-  const [agreed, setAgreed] = useState(false);
+  const [agreed, setAgreed] = useState(csAgreed);
   const [showFormError, setShowFormError] = useState(false);
   const [allAgreement, setAllAgreement] = useState(false);
   const [showChart, setShowChart] = useState(false);
@@ -513,7 +516,7 @@ function App() {
             <Col className="text-center">
               <h2 style={{ maxWidth: '600px', margin: '0 auto 1rem auto' }}>
                 {formType === 'spb'
-                  ? 'Agile Strategy Readiness'
+                  ? 'Planning Agility Benchmark'
                   : 'Marketing Work Management Benchmark'}
               </h2>
               <p>
